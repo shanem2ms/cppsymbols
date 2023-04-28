2110b884eda2b2e1ec98c6e4886598e1b6742f18
Shane Morrison <sorrison@us.ibm.com>
2023-04-28 09:17:42 -0400

    cppsymview

---
+++ cppsymview/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cppsymview
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ConnectTcp();
+        }
+
+        async void ConnectTcp()
+        {
+            IPAddress iPAddress = new IPAddress(new byte[4] { 127, 0, 0, 1 });
+            IPEndPoint ipEndPoint = new(iPAddress, 9099);
+
+            using Socket client = new(
+            ipEndPoint.AddressFamily,
+            SocketType.Stream,
+            ProtocolType.Tcp);
+
+            await client.ConnectAsync(ipEndPoint);
+            while (true)
+            {
+                // Send message.
+                var message = "Hi friends <|EOM|>";
+                var messageBytes = Encoding.UTF8.GetBytes(message);
+                _ = await client.SendAsync(messageBytes, SocketFlags.None);
+                Console.WriteLine($"Socket client sent message: \"{message}\"");
+
+                // Receive ack.
+                var buffer = new byte[1_024];
+                var received = await client.ReceiveAsync(buffer, SocketFlags.None);
+                var response = Encoding.UTF8.GetString(buffer, 0, received);
+                if (response == "<|ACK|>")
+                {
+                    Console.WriteLine(
+                        $"Socket client received acknowledgment: \"{response}\"");
+                    break;
+                }
+                // Sample output:
+                //     Socket client sent message: "Hi friends 👋!<|EOM|>"
+                //     Socket client received acknowledgment: "<|ACK|>"
+            }
+
+            client.Shutdown(SocketShutdown.Both);
+        }
+    }
+}
