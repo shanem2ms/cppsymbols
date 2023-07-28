2fecf0fe9e1a6c9893174bc6930f14952727c9d0
Shane Morrison <shanemorrison@Shanes-MacBook-Pro.local>
2023-07-28 09:50:14 -0400

    up

---
+++ buildmac.sh
@@ -0,0 +1,1 @@
+cmake -D "VCPKG_DIR"=/Users/shanemorrison/vcpkg -D "VCPKG_TARGET_TRIPLET"="arm64-osx" -D "LLVM_DIR"="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/" ..
